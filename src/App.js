8291ea91d3b672bde9ec470b2d850375bc69142a
Ygor <ygoralcantaramedeiros@gmail.com>
2019-09-16 17:02:14 -0300

    remove Radium and add CSS Modules

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
-import './App.css';
-import Radium, { StyleRoot } from 'radium';
+import classes from './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
@@ -46,20 +45,9 @@ class App extends Component {
     }
 
     render() {
-        const style = {
-            backgroundColor: 'green',
-            color: 'white',
-            font: 'inherit',
-            border: '1px solid blue',
-            padding: '8px',
-            cursor: 'pointer',
-            ':hover': {
-                backgroundColor: 'lightgreen',
-                color: 'black'
-            }
-        };
 
         let persons = null;
+        let btnClass = '';
 
         if (this.state.showPersons) {
 
@@ -77,35 +65,29 @@ class App extends Component {
                 </div>
             );
 
-            style.backgroundColor = 'red';
-            style[':hover'] = {
-                backgroundColor: 'salmon',
-                color: 'black'
-            }
+            btnClass = classes.Red;
         }
 
-        const classes = [];
+        const assignedClasses = [];
 
         if (this.state.persons.length <= 2) {
-            classes.push('red');
+            assignedClasses.push(classes.red);
         }
 
         if (this.state.persons.length <= 1) {
-            classes.push('bold');
+            assignedClasses.push(classes.bold);
         }
 
         return (
-            <StyleRoot>
-                <div className="App">
-                    <h1>Hello ReactJs</h1>
-                    <p className={classes.join(' ')}>Hi, I'm a React App</p>
-                    <button style={style} onClick={this.togglePersonsHandler}>Toggle Persons</button>
-                    {persons}
-                </div>
-            </StyleRoot>
+            <div className={classes.App}>
+                <h1>Hello ReactJs</h1>
+                <p className={assignedClasses.join(' ')}>Hi, I'm a React App</p>
+                <button className={btnClass} onClick={this.togglePersonsHandler}>Toggle Persons</button>
+                {persons}
+            </div>
         );
         //return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Hello ReactJs'));
     }
 }
 
-export default Radium(App);
+export default App;
